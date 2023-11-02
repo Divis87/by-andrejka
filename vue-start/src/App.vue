c5e1081b50fbb1382ba16ebc7f5090aa081147f0
Dávid Šimrák <david@eyerim.com>
2023-11-02 09:50:03 +0100

    by-andrejka vue form

--- vue-start/src/App.vue
+++ vue-start/src/App.vue
@@ -51,7 +51,26 @@
         <button @click="upload" class="text-green">Upload</button>
 
         <!-- Success message -->
-        <div v-if="message" class="fixed top-0 left-0 right-0 bg-green-500 text-white p-4 text-center">
+        <div v-if="message" class="transition transform fixed z-100 bottom-0 inset-x-0 pb-2 sm:pb-5 opacity-100 scale-100 translate-y-0 ease-out duration-500">
+            <div class="max-w-screen-xl mx-auto px-2 sm:px-4">
+                <div class="p-2 rounded-lg bg-gray-900 shadow-lg sm:p-3">
+                    <div class="flex items-center justify-between flex-wrap">
+                        <div class="w-0 flex-1 flex items-center">
+                            <img class="h-6" src="/_next/static/media/tailwind-ui-logo-on-dark.e075f076d1193a2062dc60571c75a1d2.svg" alt="">
+                            <p class="ml-3 font-medium text-white truncate">
+                                <strong>{{ message }}</strong>
+                            </p>
+                        </div>
+                        <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
+                            <button @click="hideMessage" type="button" class="-mr-1 flex p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-gray-800" aria-label="Hide banner">
+                                <svg class="h-6 w-6 text-white" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <strong>{{ message }}</strong>
         </div>
     </div>
@@ -105,7 +124,7 @@ export default {
                 this.message = response.data.message;
                 console.log('File uploaded successfully.');
 
-                // Set a timer to automatically hide the message after 5 seconds
+                // Set a timer to automatically hide the message after 10 seconds
                 setTimeout(() => {
                     this.message = '';
                 }, 10000);
@@ -113,6 +132,9 @@ export default {
                 console.error(err);
                 this.message = 'An error occurred while uploading the file.';
             });
+        },
+        hideMessage() {
+            this.message = '';
         }
     }
 }
