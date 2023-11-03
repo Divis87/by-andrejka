4acc0ead05a722f00f95e5dd5c444c2fb2da7115
Dávid Šimrák <david@eyerim.com>
2023-11-03 20:19:17 +0100

    by-andrejka vue form

--- vue-start/src/App.vue
+++ vue-start/src/App.vue
@@ -1,6 +1,40 @@
 <template>
+    <div>
+        <button @click="openModal" class="bg-blue-500 text-white p-2">
+            Otvoriť modálne okno
+        </button>
+        <div v-if="showModal" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+            <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                        <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                            <div class="sm:flex sm:items-start">
+                                <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                    <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                                    </svg>
+                                </div>
+                                <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                    <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Deactivate account</h3>
+                                    <div class="mt-2">
+                                        <p class="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                            <button type="button" @click="closeModal" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Deactivate</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
     <div class="bg-orange p-10">
-        <div class="step-1">
+        <div class="step-1" v-show="currentStep === 'step-1'">
             <div class="mb-5">
                 <div>
                     <label for="item1">Item 1</label>
@@ -11,9 +45,13 @@
                     <input type="radio" id="item2" value="item2" v-model="selectedItem">
                 </div>
             </div>
+            <button @click="nextStep('step-2')" v-show="currentStep === 'step-1'">Continue</button>
         </div>
 
-        <div class="step-2">
+        <div class="step-2" v-show="currentStep === 'step-2'">
+            <div class="mb-5">
+                <button @click="previousStep('step-1')" v-show="currentStep === 'step-2'">Back</button>
+            </div>
             <div class="mb-5">
                 <div>
                     <label for="gender-item1">Mužské</label>
@@ -31,9 +69,13 @@
             <div class="mb-5">
                 <input type="text" v-model="size" placeholder="Veľkosť">
             </div>
+            <button @click="nextStep('step-3')" v-show="currentStep === 'step-2'">Continue</button>
         </div>
 
-        <div class="step-3">
+        <div class="step-3" v-show="currentStep === 'step-3'">
+            <div class="mb-5">
+                <button @click="previousStep('step-2')" v-show="currentStep === 'step-3'">Back</button>
+            </div>
             <div class="mb-5">
                 <div class="w-full">
                     <div>
@@ -52,9 +94,13 @@
                     </label>
                 </div>
             </div>
+            <button @click="nextStep('step-4')" v-show="currentStep === 'step-3'">Continue</button>
         </div>
 
-        <div class="step-4">
+        <div class="step-4" v-show="currentStep === 'step-4'">
+            <div class="mb-5">
+                <button @click="previousStep('step-3')" v-show="currentStep === 'step-4'">Back</button>
+            </div>
             <div class="mb-5">
                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Vaše celé meno:</label>
                 <div class="relative">
@@ -118,6 +164,7 @@ import axios from 'axios';
 export default {
     data() {
         return {
+            currentStep: 'step-1',
             showModal: false,
             name: '',
             password: '',
@@ -133,6 +180,13 @@ export default {
     },
 
     methods: {
+        nextStep(step) {
+            // You can add validation logic here if needed
+            this.currentStep = step;
+        },
+        previousStep(step) {
+            this.currentStep = step;
+        },
         openModal() {
             this.showModal = true;
         },
