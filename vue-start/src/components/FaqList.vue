24bab949fe012f5ca981cfda0a8ab5424af422a1
Dávid Šimrák <david@eyerim.com>
2023-11-09 13:45:50 +0100

    by-andrejka vue form

--- vue-start/src/components/FaqList.vue
+++ vue-start/src/components/FaqList.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <details v-for="(faq, index) in faqItems" :key="index" class="leading-7 text-gray-700 border-0 border-b-2">
+        <details v-for="(faq, index) in faqItems" :key="index" class="leading-8 text-gray-700 border-0 border-b-2">
             <summary @click="toggleDetails(index)" class="font-semibold py-5 border-gray-200 text-lg cursor-pointer list-none flex justify-between items-center transition hover:text-orange">
                 <span>{{ faq.question }}</span>
                 <i class="fas fa-chevron-down text-medium ml-3 transform transition-transform" :class="{ 'rotate-180': faq.isOpen }"></i>
